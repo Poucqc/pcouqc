942710d5d65c5a55e368635d71639535320f544e
pcouqc <163396796+Poucqc@users.noreply.github.com>
2024-04-04 13:27:33 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+SELECT concat('/home/grep/src/',f.BOARD_ID,'/',f.FILE_ID,f.FILE_NAME,f.FILE_EXT)
+from USED_GOODS_BOARD b join USED_GOODS_FILE f on b.BOARD_ID=f.BOARD_ID
+where VIEWS=(select max(VIEWS) from USED_GOODS_BOARD)
+order by f.FILE_ID desc
