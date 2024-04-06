78dd60d50637155d12a7d2e870e8fa0f802678c0
pcouqc <163396796+Poucqc@users.noreply.github.com>
2024-04-06 21:25:55 +0900

    [level 0] Title: 두 수의 차, Time: 0.01 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차/두 수의 차.kt
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.kt
@@ -1,6 +1,8 @@
 class Solution {
     fun solution(num1: Int, num2: Int): Int {
-        var answer: Int = num1 - num2
+        val lmtnum1 = if (num1 < -50000) -50000 else if (num1 > 50000) 50000 else num1
+        val lmtnum2 = if (num2 < -50000) -50000 else if (num2 > 50000) 50000 else num2
+        var answer: Int = lmtnum1-lmtnum2
         return answer
     }
 }
