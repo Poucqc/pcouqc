a4bf97e20f23e721456bce7d8eacc2b73a48b5d4
pcouqc <163396796+Poucqc@users.noreply.github.com>
2024-04-19 12:47:08 +0900

    [level 1] Title: 약수의 합, Time: 7.85 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.kt
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.kt
@@ -1,22 +1,5 @@
 class Solution {
-    fun sumAllDivisors(number: Int): Int {
-        if (number < 0 || number > 3000) {
-            println("입력값을 확인해 주세요")
-            return 0
-        } else if (number == 0) {
-            return 0
-        }
-        
-        val divisors = mutableListOf<Int>()
-        for (n in 1..number) {
-            if (number % n == 0) {
-                divisors.add(n)
-            }
-        }
-        return divisors.sum()
-    }
-    
-    fun solution(number: Int): Int {
-        return sumAllDivisors(number)
+    fun solution(n: Int): Int {
+        return (1..n).filter{ n % it == 0 }.sum()
     }
 }
