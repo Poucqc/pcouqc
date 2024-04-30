9bf0338e1c8185d4933be6937613cf9042181f42
pcouqc <163396796+Poucqc@users.noreply.github.com>
2024-04-30 09:55:19 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 11.45 ms, Memory: 61.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(s: String): String {
+        return if (s.length % 2 == 0) {
+            s.substring(s.length/2-1..s.length/2)
+        } else {
+            s.substring(s.length/2..s.length/2)
+        }
+    }
+}
